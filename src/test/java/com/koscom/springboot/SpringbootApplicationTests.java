b3f0cb09f50399e31afaf9530165dbccc5eb039e
maxchoe <maxchoe.dnc@gmail.com>
2021-09-07 10:49:46 +0900

    프로젝트 생성

---
+++ src/test/java/com/koscom/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.koscom.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//아래의 annotation은 test에 관련된 로직을 사용하겠다라는 뜻이된다.
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
